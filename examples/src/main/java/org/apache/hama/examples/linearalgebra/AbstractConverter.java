0eea7fcad78adee32900ca896eb9236e2d320abe
Mikalaj Parafeniuk <physmatman@gmail.com>
2012-06-08 14:12:17 +0300

    some files deleted

--- examples/src/main/java/org/apache/hama/examples/linearalgebra/AbstractConverter.java
+++
@@ -1,6 +0,0 @@
-package org.apache.hama.examples.linearalgebra;
-
-public abstract class AbstractConverter <F extends AbstractMatrixFormat, S extends AbstractMatrixFormat> {
-  public abstract S convertForward(F f);
-  public abstract F convertBack(S f);
-}
